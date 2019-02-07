832ccc506911e365187a98e2763f566cef9d78a4
hendraronaldi <hendraronaldi@hotmail.com>
2019-02-07 20:56:30 +0700

    add feature extraction

--- feature.py
+++ feature.py
@@ -1,3 +1,18 @@
 # TODO: Add feature extraction
+import os.path
+import pickle
+
 def featureExtraction(data):
-    pass
+    data.columns = ['tweet']
+    
+    inputTweet = []
+    i = 0
+    for twt in data.iterrows():
+        #buang white space, simpan dalam list
+        inputTweet.append(unicode(data['tweet'][i],errors='ignore'))
+        i = i + 1
+    
+    vectorizer = pickle.load(open('vectorizer.pk','rb'))
+    x = vectorizer.transform(inputTweet)
+    
+    return x
