7d74259799c98ebbeff13d7a6dd98dc5aabaa8c3
hendraronaldi <hendraronaldi@hotmail.com>
2019-02-07 14:46:43 +0700

    save model

--- ml.py
+++ ml.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
+import pickle
+
+from preprocess import preProcess
 
 def trainingModel():
     train_data = pd.read_csv('database/train_data.csv')
     test_data = pd.read_csv('database/test_data.csv')
     model = "INSERT MODEL HERE"
-    return model
+    # train model
+
+    # save model
+    model_filename = 'final_model.sav'
+    pickle.dump(model, open(model_filename, 'wb'))
 
 def predictInput(data):
-    model = trainingModel()
+    model_filename = 'final_model.sav'
+    # model = pickle.load(open(model_filename, 'rb'))
     # prediction = model.predict(data.Tweet)
     # return prediction
