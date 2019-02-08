1fa2adfe0cffd4971823d001744653802aed03fe
hendraronaldi <hendraronaldi@hotmail.com>
2019-02-08 22:11:53 +0700

    predict using trained models

--- predict.py
+++ predict.py
@@ -1,8 +1,17 @@
 import pickle
+import pandas as pd
 
-def predictInput(data):
-    model_filename = 'final_model.sav'
-    model = pickle.load(open(model_filename, 'rb'))
-    predictions = model.predict(data.tweet)
-    data.label = predictions
+def predictInput(tweets):
+    gpc = pickle.load(open('gpc_model.sav', 'rb'))
+    dt = pickle.load(open('dt_model.sav', 'rb'))
+    mlp = pickle.load(open('mlp_model.sav', 'rb'))
+    y_pred_gpc = gpc.predict(tweets)
+    y_pred_dt = dt.predict(tweets)
+    y_pred_mlp = mlp.predict(tweets)
+    predictions = {
+        'gpc_pred': y_pred_gpc,
+        'dt_pred': y_pred_dt,
+        'mlp_pred': y_pred_mlp
+    }
+    data = pd.DataFrame(data=predictions)
     return data
