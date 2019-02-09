0bec3d0ce907bd43ccf1cd362e41fbe129e72aa4
hendraronaldi <hendraronaldi@hotmail.com>
2019-02-09 08:31:40 +0700

    remove emoji from string

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 from normalize import wordNormalize
 import numpy as np
 import pandas as pd
+import re, string
 
 # TODO: Add all pre process data in this function
 def preProcess(data):
@@ -37,10 +38,18 @@ def removeMissingValue(data):
 # get words from tweets
 def removeUneccessaryCharactersAndWords(data):
     # input dataframe
+    regex = re.compile('[%s]' % re.escape(string.punctuation))
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+
     tweets = data.tweet
     cleaned_tweets = []
     for tweet in tweets:
-        words = [word for word in tweet.split(' ') if (len(word) > 0 and word[0] != '@' and word[0] != '#' and word[0:2] != 'RT' and word[0:4] != 'http')]
+        words = [emoji_pattern.sub(r'', regex.sub('', word)) for word in tweet.split(' ') if (len(word) > 0 and not word.startswith('@') and not word.startswith('#') and not word.startswith('RT') and not word.startswith('http'))]
         cleaned_tweets.append(' '.join(words))
     data.tweet = cleaned_tweets
     return data # output dataframe
